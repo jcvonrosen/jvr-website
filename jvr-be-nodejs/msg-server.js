ff253c3e99a46d245b36fba04f36b5aeebfd669b
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2026-04-14 21:31:54 -0400

    setup msg-server node js backend

--- jvr-be-nodejs/msg-server.js
+++ jvr-be-nodejs/msg-server.js
@@ -1,6 +1,14 @@
+require("dotenv").config();
+import { Resend } from "resend";
+
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const forwardingEmail = process.env.FORWARDING_EMAIL;
+const resend = new Resend("re_xxxxxxxxx");
+
+app.use(cors());
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
@@ -9,7 +17,29 @@ app.listen(port, () => {
 });
 
 app.post("/message", (req, res) => {
+	if (!req.is("application/json")) {
+		return res.status(400).send("Expected application/json");
+	}
+
 	console.log("Received message:", req.body);
+	const { contactForm } = req.body;
+
+	(async () => {
+		const { data, error } = await resend.emails.send({
+			from: `${contactForm.name} <${contactForm.email}>`,
+			to: [forwardingEmail],
+			subject: contactForm.subject,
+			text: `You have received a new message from ${contactForm.name} (${contactForm.email}):\n\n${contactForm.message}`,
+			html: "<strong>It works!</strong>",
+		});
+
+		if (error) {
+			return console.error({ error });
+		}
+
+		console.log({ data });
+	})();
+
 	res.send("Message received");
 });
 
@@ -17,6 +47,3 @@ app.options("/message", (req, res) => {
 	res.set("Allow", "POST, OPTIONS");
 	res.sendStatus(204);
 });
-
-var message = "hello world";
-console.log(message);
